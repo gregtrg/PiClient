ad903bf365d5459bb6b6e62344410ce153fd8d34
gregtrg <lgregltrg@gmail.com>
2015-10-14 14:34:02 +0300

    14.10.15 Обновлены javadocs

--- src/main/java/Client/DAO/IDaoProceedDataObject.java
+++ src/main/java/Client/DAO/IDaoProceedDataObject.java
@@ -6,14 +6,19 @@ import java.sql.ResultSet;
 
 
 /**
- * Класс DAO для ProceedDataObject
+ * Класс для работы с базой данных
  * @author Grigorii Moiseev
  */
 public interface IDaoProceedDataObject {
-    /** Возвращает объект, содержащий информацию для PI */
+    /** Запрашивает информцию из базы и возвращает объект, содержащий эту информацию.
+     * @return объект, содержащий информацию для PI */
     public ProceedDataObject getProceedDataObject() throws PersistException;
-    /** Возвращает запрос на выборку */
+    /** Передает заготовленный запрос на языке SQL.
+     * @return запрос на выборку */
     public String getSelectedQuery();
-    /** Возвращает объект ProceedDataObject содержащий инфомацию */
+    /** Приводит данные в вид, воспринимаемый JSON Simple.
+     * @param rs выборка данных.
+     * @return объект ProceedDataObject содержащий инфомацию из базы,<br>
+     *     для дальнейшего преобразования в JSON объект*/
     public ProceedDataObject parseResultSet(ResultSet rs) throws PersistException;
 }
